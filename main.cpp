65dd90fed543952b067fcc9131df1cf35af6b7df
jstevens8185 <j.stevens8185@gmail.com>
2022-12-23 00:55:30 -0600

    Add new file for location class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <random>
+#include "Coordinates.cpp"
 
 #define _NORTHWALL = 50;
 #define _SOUTHWALL = -50;
@@ -9,51 +10,7 @@
 #define _FLOOR = 0;
 #define _CEILING = 100;
 
-class LocationCoordinates {
-	private:
-		float xPosition;
-		float yPosition;
-		float zPosition;
-	public:
-		LocationCoordinates(){
-			xPosition = 0;
-			yPosition = 0;
-			zPosition = 0;
-		}
-		LocationCoordinates(float x, float y){
-			xPosition = x;
-			yPosition = y;
-			zPosition = 0;
-		}
-		LocationCoordinates(float x, float y, float z){
-			xPosition = x;
-			yPosition = y;
-			zPosition = z;
-		}
-		void setXPosition(float x){
-			xPosition = x;
-		}
-		void setYPosition(float y){
-			yPosition = y;
-		}
-		void setZPosition(float z){
-			zPosition = z;
-		}
-		float getXPosition(){
-			return xPosition;
-		}
-		float getYPosition(){
-			return yPosition;
-		}
-		float getZPosition(){
-			return zPosition;
-		}
-		void updatePosition(float x, float y, float z){
-			xPosition += x;
-			yPosition += y;
-			zPosition +=z;
-		}
-};
+
 
 enum SpriteClass {TANK, BOSS, DPS, MEDIC, MINION, OPERATOR};
 
